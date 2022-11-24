f05c5917a114535ba43d28a571016a2be5dec195
KLIPE_LD <klipe.ld@gmail.com>
2022-11-24 11:34:22 +0100

    2022-11-24

--- app/Services/ArticleService.php
+++ app/Services/ArticleService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Article;
+use Illuminate\Support\Facades\File; 
 
 class ArticleService
 {
@@ -13,4 +14,21 @@ class ArticleService
         $this->article = $article;
     }
 
+    public function uploadedImage($image): string
+    {
+        if (!is_null($image)) {
+            $imgName = time().'_'.rand().'.'.$image->extension();
+            $image->move(public_path('img/articles'), $imgName);
+            return $imgName;
+        }
+    }
+
+    public function deletedImage($image): void
+    {
+        $image = public_path('img/articles/'.$image);
+        if(File::exists($image)) 
+        {
+            File::delete($image);
+        }
+    }
 }
